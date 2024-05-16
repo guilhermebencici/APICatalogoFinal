4282c284e24dc329e3753b355fed4a314faff378
GuilhermeSouza <guilhermebencici@gmail.com>
2024-05-16 17:27:18 -0300

    Criação das models

---
+++ APICatalogoFinal/Models/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+
+// está é uma classe Anêmica, pois não tem comportamento, apenas propriedades
+
+namespace APICatalogoFinal.Models
+{
+    public class Categoria
+    {
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
+        public int CategoriaId { get; set; }
+        public string? Nome { get; set; }
+        public string? ImagemUrl { get; set; }
+
+        public ICollection<Produto>? Produtos { get; set; }
+    }
+}
