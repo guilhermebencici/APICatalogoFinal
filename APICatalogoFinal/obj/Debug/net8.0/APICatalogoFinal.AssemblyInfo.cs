f9b762b32021d455a53aa616f5193663d980c491
GuilhermeSouza <guilhermebencici@gmail.com>
2024-05-17 14:57:38 -0300

    configurando chamada dos serviços -program-

--- APICatalogoFinal/obj/Debug/net8.0/APICatalogoFinal.AssemblyInfo.cs
+++ APICatalogoFinal/obj/Debug/net8.0/APICatalogoFinal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogoFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67768fc46ff15312e7c27caa8695db91ae65118f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28e5c7166c171f7d7ddd3702e4065639a4528097")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogoFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogoFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
